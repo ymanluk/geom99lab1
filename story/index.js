0fe143bdf352141555c34f25b2c407979faa7b1c
ymanluk <146376058+ymanluk@users.noreply.github.com>
2024-01-19 14:57:56 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -4,68 +4,36 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// [START maps_streetview_overlays]
-let panorama: google.maps.StreetViewPanorama;
+// [START maps_polyline_simple]
+// This example creates a 2-pixel-wide red polyline showing the path of
+// the first trans-Pacific flight between Oakland, CA, and Brisbane,
+// Australia which was made by Charles Kingsford Smith.
 
 function initMap(): void {
-  const astorPlace = { lat: 40.729884, lng: -73.990988 };
-
-  // Set up the map
   const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
     {
-      center: astorPlace,
-      zoom: 18,
-      streetViewControl: false,
+      zoom: 3,
+      center: { lat: 0, lng: -180 },
+      mapTypeId: "terrain",
     }
   );
 
-  document
-    .getElementById("toggle")!
-    .addEventListener("click", toggleStreetView);
-
-  // Set up the markers on the map
-  const cafeMarker = new google.maps.Marker({
-    position: { lat: 40.730031, lng: -73.991428 },
-    map,
-    icon: "https://chart.apis.google.com/chart?chst=d_map_pin_icon&chld=cafe|FFFF00",
-    title: "Cafe",
+  const flightPlanCoordinates = [
+    { lat: 37.772, lng: -122.214 },
+    { lat: 21.291, lng: -157.821 },
+    { lat: -18.142, lng: 178.431 },
+    { lat: -27.467, lng: 153.027 },
+  ];
+  const flightPath = new google.maps.Polyline({
+    path: flightPlanCoordinates,
+    geodesic: true,
+    strokeColor: "#FF0000",
+    strokeOpacity: 1.0,
+    strokeWeight: 2,
   });
 
-  const bankMarker = new google.maps.Marker({
-    position: { lat: 40.729681, lng: -73.991138 },
-    map,
-    icon: "https://chart.apis.google.com/chart?chst=d_map_pin_icon&chld=dollar|FFFF00",
-    title: "Bank",
-  });
-
-  const busMarker = new google.maps.Marker({
-    position: { lat: 40.729559, lng: -73.990741 },
-    map,
-    icon: "https://chart.apis.google.com/chart?chst=d_map_pin_icon&chld=bus|FFFF00",
-    title: "Bus Stop",
-  });
-
-  // We get the map's default panorama and set up some defaults.
-  // Note that we don't yet set it visible.
-  panorama = map.getStreetView()!; // TODO fix type
-  panorama.setPosition(astorPlace);
-  panorama.setPov(
-    /** @type {google.maps.StreetViewPov} */ {
-      heading: 265,
-      pitch: 0,
-    }
-  );
-}
-
-function toggleStreetView(): void {
-  const toggle = panorama.getVisible();
-
-  if (toggle == false) {
-    panorama.setVisible(true);
-  } else {
-    panorama.setVisible(false);
-  }
+  flightPath.setMap(map);
 }
 
 declare global {
@@ -74,5 +42,5 @@ declare global {
   }
 }
 window.initMap = initMap;
-// [END maps_streetview_overlays]
+// [END maps_polyline_simple]
 export {};
