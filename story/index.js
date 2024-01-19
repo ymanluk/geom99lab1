095a05178ab0618a70507e0db53e646c7a8339ea
ymanluk <146376058+ymanluk@users.noreply.github.com>
2024-01-18 21:59:20 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -1,32 +1,31 @@
-<html>
-  <head>
-    <title>Street View split-map-panes</title>
-    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-    <!-- playground-hide -->
-    <script>
-      const process = { env: {} };
-      process.env.GOOGLE_MAPS_API_KEY =
-        "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg";
-    </script>
-    <!-- playground-hide-end -->
+function initialize() {
+  const fenway = { lat: 42.345573, lng: -71.098326 };
+  const map = new google.maps.Map(
+    document.getElementById("map") as HTMLElement,
+    {
+      center: fenway,
+      zoom: 14,
+    },
+  );
+  const panorama = new google.maps.StreetViewPanorama(
+    document.getElementById("pano") as HTMLElement,
+    {
+      position: fenway,
+      pov: {
+        heading: 34,
+        pitch: 10,
+      },
+    },
+  );
 
-    <link rel="stylesheet" type="text/css" href="./style.css" />
-    <script type="module" src="./index.js"></script>
-  </head>
-  <body>
-    <div id="map"></div>
-    <div id="pano"></div>
+  map.setStreetView(panorama);
+}
 
-    <!-- 
-      The `defer` attribute causes the callback to execute after the full HTML
-      document has been parsed. For non-blocking uses, avoiding race conditions,
-      and consistent behavior across browsers, consider loading using Promises.
-      See https://developers.google.com/maps/documentation/javascript/load-maps-js-api
-      for more information.
-      -->
-    <script
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&callback=initialize&v=weekly"
-      defer
-    ></script>
-  </body>
-</html>
+declare global {
+  interface Window {
+    initialize: () => void;
+  }
+}
+window.initialize = initialize;
+
+export {};
