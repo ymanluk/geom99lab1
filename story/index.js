6590af8c506419658f5281333211811f454ffe80
ymanluk <146376058+ymanluk@users.noreply.github.com>
2024-01-19 15:23:57 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -3,7 +3,7 @@
 // Australia which was made by Charles Kingsford Smith.
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 12,
+    zoom: 16,
     center: { lat: 43.734458, lng: 7.421363 },
   });
   const CircuitCoordinates = [
@@ -11,6 +11,12 @@ function initMap() {
     { lat: 43.73575, lng: 7.42125 },
     { lat: 43.73659, lng: 7.42150 },
     { lat: 43.73693, lng: 7.42183 },
+    { lat: 43.73706, lng: 7.42233 },
+    { lat: 43.73709, lng: 7.42291 },
+    { lat: 43.73, lng: 7.42 },
+    { lat: 43.73, lng: 7.42 },
+    { lat: 43.73, lng: 7.42 },
+    
   ];
   const flightPath = new google.maps.Polyline({
     path: CircuitCoordinates,
