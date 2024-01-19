dc32fe7067984355e1534aa083426abc23ad2458
ymanluk <146376058+ymanluk@users.noreply.github.com>
2024-01-19 15:15:38 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -4,17 +4,16 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 3,
-    center: { lat: 0, lng: -180 },
-    mapTypeId: "terrain",
+    center: { lat: 43.734458, lng: 7.421363 },
   });
-  const flightPlanCoordinates = [
+  const CircuitCoordinates = [
     { lat: 37.772, lng: -122.214 },
     { lat: 21.291, lng: -157.821 },
     { lat: -18.142, lng: 178.431 },
     { lat: -27.467, lng: 153.027 },
   ];
   const flightPath = new google.maps.Polyline({
-    path: flightPlanCoordinates,
+    path: s||iGonhl@,
     geodesic: true,
     strokeColor: "#FF0000",
     strokeOpacity: 1.0,
