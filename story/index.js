b174bca7db806902ad2e2ce9bff87dd9eb6537b0
ymanluk <146376058+ymanluk@users.noreply.github.com>
2024-01-22 20:33:23 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -84,59 +84,7 @@ function initMap() {
 
   CircuitLayout.setMap(map);
 
-//adding markers attempt
-  const { Map, InfoWindow } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
-  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
-  // Set LatLng and title text for the markers. The first marker (Boynton Pass)
-  // receives the initial focus when tab is pressed. Use arrow keys to
-  // move between markers; press tab again to cycle through the map controls.
-  const tourStops = [
-        {
-            position: { lat: 43.734458, lng: 7.421363 }, 
-            title: "Race Start"
-        },
-        {
-            position: { lat: 34.8559195, lng: -111.7988186 }, 
-            title: "Airport Mesa"
-        },
-        {
-            position: { lat: 34.832149, lng: -111.7695277 }, 
-            title: "Chapel of the Holy Cross"
-        },
-        {
-            position: { lat: 34.823736, lng: -111.8001857 }, 
-            title: "Red Rock Crossing"
-        },
-        {
-            position: { lat: 34.800326, lng: -111.7665047 }, 
-            title: "Bell Rock"
-        },
-    ];
 
-  // Create an info window to share between markers.
-  const infoWindow = new InfoWindow();
-
-  // Create the markers.
-  tourStops.forEach(({position, title}, i) => {
-        const pin = new PinElement({
-            glyph: `${i + 1}`,
-        });
-
-        const marker = new AdvancedMarkerElement({
-            position,
-            map,
-            title: `${i + 1}. ${title}`,
-            content: pin.element,
-        });
-
-        // Add a click listener for each marker, and set up the info window.
-        marker.addListener('click', ({ domEvent, latLng }) => {
-            const { target } = domEvent;
-            infoWindow.close();
-            infoWindow.setContent(marker.title);
-            infoWindow.open(marker.map, marker);
-        });
-    });
 
   
 }
